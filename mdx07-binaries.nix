e805ba28a4ce94ffc2aa0bc3794b01efde4c660c
Oli <oli@olillin.com>
2026-01-27 21:28:19 +0100

    Add simserver desktop entry

--- mdx07-binaries.nix
+++ mdx07-binaries.nix
@@ -14,9 +14,18 @@
   pango,
   fontconfig,
   libusb1,
+  makeDesktopItem,
+  imagemagick,
   ...
 }: let
   hash = "2597eab47fe4a58a1392c682976a1b04a23b7462";
+
+  desktopItem = makeDesktopItem {
+    name = "simserver";
+    desktopName = "Simserver";
+    icon = "simserver";
+    exec = "simserver";
+  };
 in
   stdenv.mkDerivation rec {
     pname = "mdx07-binaries";
@@ -27,7 +36,7 @@ in
       sha256 = "sha256-uKw8r1KC3QWp9bFy8YmJcl/9FC5z6Ssv3BAfmzl1CMQ=";
     };
 
-    nativeBuildInputs = [autoPatchelfHook];
+    nativeBuildInputs = [autoPatchelfHook imagemagick];
 
     buildInputs = [
       stdenv.cc.cc.lib
@@ -46,8 +55,21 @@ in
 
     # Only the linux‑x64 dir is needed for x86_64‑linux
     installPhase = ''
+      runHook preInstall
+
       mkdir -p $out/bin
       cp -R linux-x64/* $out/bin/
+
+      runHook postInstall
+    '';
+
+    postInstall = ''
+      install -m 444 -D "${desktopItem}/share/applications/"* \
+        -t $out/share/applications/
+      for size in 16 24 32 48 64 128; do
+        mkdir -p $out/share/icons/hicolor/"$size"x"$size"/apps
+        convert -background none -resize "$size"x"$size" ${./simserver.png} $out/share/icons/hicolor/"$size"x"$size"/apps/simserver.png
+      done
     '';
 
     meta = with lib; {
